c03f4a742fcbe10482d1457679ffe3dc3ac685c3
aalexis.ab <aalexis.ab@gmail.com>
2023-05-01 19:16:10 +0200

    ok

--- src/ftp_data.rs
+++ src/ftp_data.rs
@@ -3,23 +3,23 @@ use std::{
     net::{TcpListener, TcpStream, SocketAddr, Ipv4Addr},
 };
 
-#[allow(unused_must_use)]
-pub struct Ftp_data{
+#[allow(dead_code)]
+pub struct FtpData{
     addr: SocketAddr,
     listener: TcpListener,
     stream: TcpStream,
     client: SocketAddr,
 }
 
-impl Ftp_data{
-    pub fn new(port:u16) -> Ftp_data {
+impl FtpData{
+    pub fn new(port:u16) -> FtpData {
         let ip_arr: Vec<u8> = dotenv!("LHOST").split('.').into_iter().map(|x| x.parse::<u8>().unwrap()).collect();
         let ip = Ipv4Addr::new(ip_arr[0],ip_arr[1],ip_arr[2],ip_arr[3]);
         let addr = SocketAddr::from((ip, port));
         let listener = TcpListener::bind(addr).unwrap();
         let stream = listener.accept().unwrap();
         
-        Ftp_data { addr: addr, listener: listener, stream: stream.0, client: stream.1}
+        FtpData { addr: addr, listener: listener, stream: stream.0, client: stream.1}
     }
     
     #[allow(unused_must_use)]
