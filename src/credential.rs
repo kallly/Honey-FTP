a2a4b3050224399d900ff7ee89b9445d476a69fa
aalexis.ab <aalexis.ab@gmail.com>
2023-05-01 19:19:33 +0200

    ok

--- src/credential.rs
+++ src/credential.rs
@@ -1,13 +1,15 @@
-#[derive(Clone)]
+#[allow(dead_code)]
 pub struct Credential{
     username: String,
     password: String,
 }
 
 impl Credential{
+    #[allow(dead_code)]
     pub fn new(username:String, password:String) -> Credential {
         Credential { username: username, password: password }
     }
+    #[allow(dead_code)]
     pub fn compare(&self, username:&String, password:&String) -> bool {
         self.username.eq(username) && self.password.eq(password)
     }
